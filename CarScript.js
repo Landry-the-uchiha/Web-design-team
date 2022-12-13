2108a86f60b6cebd6a2bfa328165ca1e013b5f21
Foofoo99-fccc <112097785+Foofoo99@users.noreply.github.com>
2022-12-13 10:59:23 -0500

    javascript

---
+++ CarScript.js
@@ -0,0 +1,8 @@
+function FordF150() 
+{
+	document.getElementById("image").src = "images/FordF150.jpg";
+	document.getElementById("description").innerHTML = "2019 Ford F-150";
+	document.getElementById("color").innerHTML = "Dark Gray";
+	document.getElementById("condition").innerHTML = "Lightly Used (1-4 Years, No Accidents)";
+	document.getElementById("cost").innerHTML = "$19 Fortnite Card";
+}
