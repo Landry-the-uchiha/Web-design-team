a95ebd9053a4c51a601b2cd2382be4071861535c
Foofoo99-fccc <112097785+Foofoo99@users.noreply.github.com>
2022-12-20 11:31:19 -0500

    got some javascript done

--- CarScript.js
+++ CarScript.js
@@ -4,5 +4,37 @@ function FordF150()
 	document.getElementById("description").innerHTML = "2019 Ford F-150";
 	document.getElementById("color").innerHTML = "Dark Gray";
 	document.getElementById("condition").innerHTML = "Lightly Used (1-4 Years, No Accidents)";
-	document.getElementById("cost").innerHTML = "$19 Fortnite Card";
+	document.getElementById("cost").innerHTML = "$42,999";
+}
+function Corolla() 
+{
+	document.getElementById("image").src = "images/1999Corolla.png";
+	document.getElementById("description").innerHTML = "1999 Toyota Corolla";
+	document.getElementById("color").innerHTML = "Sandrift Metallic";
+	document.getElementById("condition").innerHTML = "Very Used (15+ Years, >1 Accidents)";
+	document.getElementById("cost").innerHTML = "$1,499";
+}
+function Charger() 
+{
+	document.getElementById("image").src = "images/06Charger.jpg";
+	document.getElementById("description").innerHTML = "2006 Dodge Charger";
+	document.getElementById("color").innerHTML = "Yellow";
+	document.getElementById("condition").innerHTML = "Used (15+ Years, 0 Accidents)";
+	document.getElementById("cost").innerHTML = "$7,999";
+}
+function Highlander() 
+{
+	document.getElementById("image").src = "images/Highlander.webp";
+	document.getElementById("description").innerHTML = "2014 Toyota Highlander";
+	document.getElementById("color").innerHTML = "Silver";
+	document.getElementById("condition").innerHTML = "Lightly Used (5-10 Years, 0 Accidents)";
+	document.getElementById("cost").innerHTML = "$16,749";
+}
+function Jacob()
+{
+	document.getElementById("image").src = "images/FordF150.jpg";
+	document.getElementById("fullname").innerHTML = "Jacob Smith Kern";
+	document.getElementById("experience").innerHTML = "4 Years";
+	document.getElementById("phone").innerHTML = "(202) 456-1111";
+	document.getElementById("email").innerHTML = "jskern@markscarlot.com";
 }
